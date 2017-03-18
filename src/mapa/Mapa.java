427ffa597743612bcd51a1480198f645b1745457
Junior Rottava <juniorrottava@hotmail.com>
2017-03-18 18:10:38 -0300

    Correção: modificação salas.

--- src/mapa/Mapa.java
+++ src/mapa/Mapa.java
@@ -32,47 +32,31 @@ public class Mapa {
             Scanner scanner = new Scanner(SALAS);
             while(id <= TAMANHOSALAS){
                 String[] parametros;
-                parametros = scanner.nextLine().split("/");                         //DIVIDE O ARQUIVO EM BARRAS
+                parametros = scanner.nextLine().split("/");                     //DIVIDE O ARQUIVO EM BARRAS
                 //NOME/SALANORTE/CHAVE/ESTADONORTE/VALORITEM1/VALORITEM2/VALORITEM3/VALORINIMIGO1/VALORINIMIGO2/VALORINIMIGO3
-                String nome = parametros[0];                                        //NOME                                                                                     
-                Porta norte = new Porta((byte) Integer.parseInt(parametros[1]),     //PROXIMA SALA
-                                        (byte) Integer.parseInt(parametros[2]),     //ID CHAVE
-                                        (byte) Integer.parseInt(parametros[3]),     //ITEM[0]
-                                        (byte) Integer.parseInt(parametros[4]),     //ITEM[1]
-                                        (byte) Integer.parseInt(parametros[5]),     //ITEM[2]
-                                        (byte) Integer.parseInt(parametros[6]),     //INIMIGO[0]
-                                        (byte) Integer.parseInt(parametros[7]),     //INIMIGO[1]
-                                        (byte) Integer.parseInt(parametros[8]));    //INIMIGO[2]                                                                                String nome = parametros[9];                                        //NOME 
+                String nome = parametros[0];                                    //NOME                                                                                     
+                Porta norte = new Porta((byte) Integer.parseInt(parametros[1]), //PROXIMA SALA
+                                        (byte) Integer.parseInt(parametros[2]), //ID CHAVE
+                                        (byte) Integer.parseInt(parametros[3]), //ITEM
+                                        (byte) Integer.parseInt(parametros[4])); //INIMIGO                                                                        String nome = parametros[9];                                        //NOME 
 
-                Porta sul = new Porta((byte) Integer.parseInt(parametros[9]),       //PROXIMA SALA
-                                        (byte) Integer.parseInt(parametros[10]),    //ID CHAVE
-                                        (byte) Integer.parseInt(parametros[11]),    //ITEM[0]
-                                        (byte) Integer.parseInt(parametros[12]),    //ITEM[1]
-                                        (byte) Integer.parseInt(parametros[13]),    //ITEM[2]
-                                        (byte) Integer.parseInt(parametros[14]),    //INIMIGO[0]
-                                        (byte) Integer.parseInt(parametros[15]),    //INIMIGO[1]
-                                        (byte) Integer.parseInt(parametros[16]));   //INIMIGO[2]
+                Porta sul = new Porta((byte) Integer.parseInt(parametros[5]),   //PROXIMA SALA
+                                        (byte) Integer.parseInt(parametros[6]), //ID CHAVE
+                                        (byte) Integer.parseInt(parametros[7]), //ITEM
+                                        (byte) Integer.parseInt(parametros[8])); //INIMIGO
 
-                Porta leste = new Porta((byte) Integer.parseInt(parametros[17]),    //PROXIMA SALA
-                                        (byte) Integer.parseInt(parametros[18]),    //ID CHAVE
-                                        (byte) Integer.parseInt(parametros[19]),    //ITEM[0]
-                                        (byte) Integer.parseInt(parametros[20]),    //ITEM[1]
-                                        (byte) Integer.parseInt(parametros[21]),    //ITEM[2]
-                                        (byte) Integer.parseInt(parametros[22]),    //INIMIGO[0]
-                                        (byte) Integer.parseInt(parametros[23]),    //INIMIGO[1]
-                                        (byte) Integer.parseInt(parametros[24]));   //INIMIGO[2]
+                Porta leste = new Porta((byte) Integer.parseInt(parametros[9]), //PROXIMA SALA
+                                        (byte) Integer.parseInt(parametros[10]),//ID CHAVE
+                                        (byte) Integer.parseInt(parametros[11]),//ITEM
+                                        (byte) Integer.parseInt(parametros[12]));//INIMIGO
 
-                Porta oeste = new Porta((byte) Integer.parseInt(parametros[25]),    //PROXIMA SALA
-                                        (byte) Integer.parseInt(parametros[26]),    //ID CHAVE
-                                        (byte) Integer.parseInt(parametros[27]),    //ITEM[0]
-                                        (byte) Integer.parseInt(parametros[28]),    //ITEM[1]
-                                        (byte) Integer.parseInt(parametros[29]),    //ITEM[2]
-                                        (byte) Integer.parseInt(parametros[30]),    //INIMIGO[0]
-                                        (byte) Integer.parseInt(parametros[31]),    //INIMIGO[1]
-                                        (byte) Integer.parseInt(parametros[32]));   //INIMIGO[2]
+                Porta oeste = new Porta((byte) Integer.parseInt(parametros[13]),//PROXIMA SALA
+                                        (byte) Integer.parseInt(parametros[14]),//ID CHAVE
+                                        (byte) Integer.parseInt(parametros[15]),//ITEM
+                                        (byte) Integer.parseInt(parametros[16]));//INIMIGO
 
-                Sala sala = new Sala(id, nome, norte, sul, leste, oeste);           //CRIA SALA
-                salas.add(sala);                                                    //ADICIONA A LISTA DE SALAS
+                Sala sala = new Sala(id, nome, norte, sul, leste, oeste);       //CRIA SALA
+                salas.add(sala);                                                //ADICIONA A LISTA DE SALAS
                 id++;
             }
         }
--- src/mapa/Porta.java
+++ src/mapa/Porta.java
@@ -9,26 +9,18 @@ package mapa;
  *
  * @author Junior
  */
-public class Porta {
+public final class Porta {
     private boolean estado;                                                     //ESTADO DA PORTA. TRUE = ABERTO, FALSE = FECHADO
     private final byte proximaSala;                                             //ID DA PROXIMA SALA
     private final byte chave;                                                   //ID DO ITEM PARA ABRIR A PORTA, 0 = NENHUM
-    private byte item0;                                                         //ID DO ITEM0 NA PORTA, 0 = NENHUM
-    private byte item1;                                                         //ID DO ITEM1 NA PORTA, 0 = NENHUM
-    private byte item2;                                                         //ID DO ITEM2 NA PORTA, 0 = NENHUM
-    private byte inimigo0;                                                      //ID DO INIMIGO0 NA PORTA, 0 = NENHUM
-    private byte inimigo1;                                                      //ID DO INIMIGO1 NA PORTA, 0 = NENHUM
-    private byte inimigo2;                                                      //ID DO INIMIGO2 NA PORTA, 0 = NENHUM
+    private byte item;                                                          //ID DO ITEM0 NA PORTA, 0 = NENHUM
+    private byte inimigo;                                                       //ID DO INIMIGO0 NA PORTA, 0 = NENHUM
     
-    public Porta(byte proximaSala, byte chave, byte item0, byte item1, byte item2, byte inimigo0, byte inimigo1, byte inimigo2){
+    public Porta(byte proximaSala, byte chave, byte item, byte inimigo){
         this.proximaSala = proximaSala;
         this.chave = chave;
-        this.item0 = item0;
-        this.item1 = item1;
-        this.item2 = item2;
-        this.inimigo0 = inimigo0;
-        this.inimigo1 = inimigo1;
-        this.inimigo2 = inimigo2;
+        this.item = item;
+        this.inimigo = inimigo;
         testaPorta();
     }
     
@@ -36,11 +28,9 @@ public class Porta {
     
     //SETA ESTADO DA PORTA
     public void testaPorta(){
-        if(chave == 0 && inimigo0 == 0 && inimigo1 == 0 && inimigo2 == 0        //SE TODOS OS IDS FOR 0
-                && item0 == 0 && item1 == 0 && item2 == 0)
-            estado = true;                                                      //PORTA ABERTA
-        else                                                                    //SENAO
-            estado = false;                                                     //PORTA FECHADA
+        estado = chave == 0 && inimigo == 0 && item == 0; //PORTA ABERTA
+        //SENAO
+        //PORTA FECHADA
     }
     
     //RETORNA VERDADEIRO SE ABERTA, FALSO SE FECHADA
@@ -63,46 +53,24 @@ public class Porta {
             chave = 0;
     }
     
-    //RETORNA ID DO INIMIGO NA POSICAO NAVEGADOR
-    public byte getInimigo(byte navegador){
-        switch(navegador){
-            case 0: 
-                return inimigo0;
-            case 1: 
-                return inimigo1;
-            case 2: 
-                return inimigo2;
-           default:
-                return 0;
-        }
+    //RETORNA ID DO INIMIGO
+    public byte getInimigo(){
+        return inimigo;
     }
     
     //SETA INIMIGOS PARA 0 (NAO EXISTEM)
     public void setInimigo(){
-        inimigo0 = 0;
-        inimigo1 = 0;
-        inimigo2 = 0;
+        inimigo = 0;
     }
     
     //RETORNA ID DO ITEM NA POSICAO NAVEGADOR
     public byte getItem(byte navegador){
-        switch(navegador){
-            case 0: 
-                return item0;
-            case 1: 
-                return item1;
-            case 2: 
-                return item2;
-           default:
-                return 0;
-        }
+        return item;
     }
     
     //SETA ITENS PARA 0 (NAO EXISTE)
     public void setItem(){
-        item0 = 0;
-        item1 = 0;
-        item2 = 0;
+        item = 0;
     }
             
     
