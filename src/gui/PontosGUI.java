add067b1a404d42119e36c53e33c1cc389cfe78f
Wagner Loch <wagner_loch@hotmail.com>
2017-03-21 20:14:15 -0300

    PontosGUI OK
    Todos parametros incrementando e decrementando

--- src/gui/PontosGUI.java
+++ src/gui/PontosGUI.java
@@ -10,9 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
 import static main.Main.HEROI;
 
@@ -24,7 +22,17 @@ public class PontosGUI extends JFrame implements ActionListener{
  
     private JLabel jLabelForca;
     private JLabel jLabelPontos;
-    private Button btnIncrementar;
+    private JLabel jLabelAgilidade;
+    private JLabel jLabelInteligencia;
+    private JLabel jLabelResistencia;
+    private Button btnIncrementarForca;
+    private Button btnDecrementarForca;
+    private Button btnIncrementarAgilidade;
+    private Button btnDecrementarAgilidade;
+    private Button btnIncrementarInteligencia;
+    private Button btnDecrementarInteligencia;
+    private Button btnIncrementarResistencia;
+    private Button btnDecrementarResistencia;
     
     public PontosGUI () {
         initGUI();
@@ -47,19 +55,128 @@ public class PontosGUI extends JFrame implements ActionListener{
         jLabelForca.setBounds(5, 55, 100, 30);
         add(jLabelForca);
         
-        btnIncrementar = new Button ("+");
-        btnIncrementar.setBounds(105, 55, 30, 30);
-        add(btnIncrementar);
-        btnIncrementar.addMouseListener(new MouseAdapter() {
+        btnIncrementarForca = new Button ("+");
+        btnIncrementarForca.setBounds(105, 55, 30, 30);
+        add(btnIncrementarForca);
+        btnIncrementarForca.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent evt) {
                 if (evt.getClickCount() == 1) {
-                    incrementar();
+                    incrementarForca();
+                }       
+            }
+        });
+        
+        btnDecrementarForca = new Button ("-");
+        btnDecrementarForca.setBounds(145, 55, 30, 30);
+        add(btnDecrementarForca);
+        btnDecrementarForca.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                if (evt.getClickCount() == 1) {
+                    decrementarForca();
+                }       
+            }
+        });
+        
+        jLabelAgilidade = new JLabel ("Agilidade: " + HEROI.getAgilidade());
+        jLabelAgilidade.setBounds(5, 95, 100, 30);
+        add(jLabelAgilidade);
+        
+        btnIncrementarAgilidade = new Button ("+");
+        btnIncrementarAgilidade.setBounds(105, 95, 30, 30);
+        add(btnIncrementarAgilidade);
+        btnIncrementarAgilidade.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                if (evt.getClickCount() == 1) {
+                    incrementarAgilidade();
+                }       
+            }
+        });
+        
+        btnDecrementarAgilidade = new Button ("-");
+        btnDecrementarAgilidade.setBounds(145, 95, 30, 30);
+        add(btnDecrementarAgilidade);
+        btnDecrementarAgilidade.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                if (evt.getClickCount() == 1) {
+                    decrementarAgilidade();
+                }       
+            }
+        });
+        
+        jLabelInteligencia = new JLabel ("Inteligencia: " + HEROI.getInteligencia());
+        jLabelInteligencia.setBounds(5, 135, 100, 30);
+        add(jLabelInteligencia);
+        
+        btnIncrementarInteligencia = new Button ("+");
+        btnIncrementarInteligencia.setBounds(105, 135, 30, 30);
+        add(btnIncrementarInteligencia);
+        btnIncrementarInteligencia.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                if (evt.getClickCount() == 1) {
+                    incrementarInteligencia();
+                }       
+            }
+        });
+        
+        btnDecrementarInteligencia = new Button ("-");
+        btnDecrementarInteligencia.setBounds(145, 135, 30, 30);
+        add(btnDecrementarInteligencia);
+        btnDecrementarInteligencia.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                if (evt.getClickCount() == 1) {
+                    decrementarInteligencia();
+                }       
+            }
+        });
+        
+        jLabelResistencia = new JLabel ("Resistencia: " + HEROI.getResistencia());
+        jLabelResistencia.setBounds(5, 175, 100, 30);
+        add(jLabelResistencia);
+        
+        btnIncrementarResistencia = new Button ("+");
+        btnIncrementarResistencia.setBounds(105, 175, 30, 30);
+        add(btnIncrementarResistencia);
+        btnIncrementarResistencia.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                if (evt.getClickCount() == 1) {
+                    incrementarResistencia();
+                }       
+            }
+        });
+        
+        btnDecrementarResistencia = new Button ("-");
+        btnDecrementarResistencia.setBounds(145, 175, 30, 30);
+        add(btnDecrementarResistencia);
+        btnDecrementarResistencia.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                if (evt.getClickCount() == 1) {
+                    decrementarResistencia();
                 }       
             }
         });
     }
     
-    private void incrementar() {
+    private void incrementarResistencia() {
+        if (HEROI.getPontos() > 0) {
+            HEROI.setResistencia((byte) (HEROI.getResistencia()+1));
+            HEROI.setPontos((byte) (HEROI.getPontos()-1));
+            jLabelResistencia.setText("Força: "+HEROI.getResistencia());
+            jLabelPontos.setText("Pontos: "+HEROI.getPontos());
+            HEROI.geraVidaMax();
+            HEROI.geraEnergiaMax();
+        }
+    }
+    
+    private void decrementarResistencia() {
+            HEROI.setResistencia((byte) (HEROI.getResistencia()-1));
+            HEROI.setPontos((byte) (HEROI.getPontos()+1));
+            jLabelResistencia.setText("Força: "+HEROI.getResistencia());
+            jLabelPontos.setText("Pontos: "+HEROI.getPontos());
+            HEROI.geraVidaMax();
+            HEROI.geraEnergiaMax();
+    }
+    
+    private void incrementarForca() {
         if (HEROI.getPontos() > 0) {
             HEROI.setForca((byte) (HEROI.getForca()+1));
             HEROI.setPontos((byte) (HEROI.getPontos()-1));
@@ -69,6 +186,57 @@ public class PontosGUI extends JFrame implements ActionListener{
             HEROI.geraEnergiaMax();
         }
     }
+    
+    private void decrementarForca() {
+            HEROI.setForca((byte) (HEROI.getForca()-1));
+            HEROI.setPontos((byte) (HEROI.getPontos()+1));
+            jLabelForca.setText("Força: "+HEROI.getForca());
+            jLabelPontos.setText("Pontos: "+HEROI.getPontos());
+            HEROI.geraVidaMax();
+            HEROI.geraEnergiaMax();
+    }
+    
+    private void incrementarAgilidade() {
+        if (HEROI.getPontos() > 0) {
+            HEROI.setAgilidade((byte) (HEROI.getAgilidade()+1));
+            HEROI.setPontos((byte) (HEROI.getPontos()-1));
+            jLabelAgilidade.setText("Agilidade: "+HEROI.getAgilidade());
+            jLabelPontos.setText("Pontos: "+HEROI.getPontos());
+            HEROI.geraVidaMax();
+            HEROI.geraEnergiaMax();
+        }
+    }
+    
+    private void decrementarAgilidade() {
+            HEROI.setAgilidade((byte) (HEROI.getAgilidade()-1));
+            HEROI.setPontos((byte) (HEROI.getPontos()+1));
+            jLabelAgilidade.setText("Agilidade: "+HEROI.getAgilidade());
+            jLabelPontos.setText("Pontos: "+HEROI.getPontos());
+            HEROI.geraVidaMax();
+            HEROI.geraEnergiaMax();
+    }
+    
+    private void incrementarInteligencia() {
+        if (HEROI.getPontos() > 0) {
+            HEROI.setInteligencia((byte) (HEROI.getInteligencia()+1));
+            HEROI.setPontos((byte) (HEROI.getPontos()-1));
+            jLabelInteligencia.setText("Inteligencia: "+HEROI.getInteligencia());
+            jLabelPontos.setText("Pontos: "+HEROI.getPontos());
+            HEROI.geraVidaMax();
+            HEROI.geraEnergiaMax();
+        }
+    }
+    
+    private void decrementarInteligencia() {
+            HEROI.setInteligencia((byte) (HEROI.getInteligencia()-1));
+            HEROI.setPontos((byte) (HEROI.getPontos()+1));
+            jLabelInteligencia.setText("Inteligencia: "+HEROI.getInteligencia());
+            jLabelPontos.setText("Pontos: "+HEROI.getPontos());
+            HEROI.geraVidaMax();
+            HEROI.geraEnergiaMax();
+    }
+    
+  
     
     @Override
     public void actionPerformed(ActionEvent ae) {
